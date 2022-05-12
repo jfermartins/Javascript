f229fc8969ff5aae4161f8bfa30d5b3dbdcb10d9
Jane Fernanda Martins <33935791+jfermartins@users.noreply.github.com>
2022-05-12 15:55:12 -0300

    Variáveis e seus valores

--- Aula-01/script.js
+++ Aula-01/script.js
@@ -1,5 +1,11 @@
 //tipos primitivos
 
+//var - escopo global e local, pode ter seu valor alterado se não tiver um valor inicial será tratada como null;
+
+//let - escopo local de bloco, pode ter seu valor alterado, se não tiver um valor inicial será tratada como null;
+
+//const - escopo local de bloco, somente leitura, o valor inicial é obrigatório e não pode ser alterado;
+
 //boolean
 var vOUF = false;
 
@@ -12,3 +18,14 @@ console.log(typeof(numeroQualquer));
 //String
 var nome = 'Jane';
 console.log(typeof(nome));
+
+//como declarar as variáveis
+
+//var
+var variavel = 'Jane';
+variavel = 'Martins';
+console.log(variavel);
+
+//let
+let variavel2 = 'Fernanda';
+console.log(variavel2);
