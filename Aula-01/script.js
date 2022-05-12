d18c4ba0529eca9f5917269807f544ebf4d0f996
Jane Fernanda Martins <33935791+jfermartins@users.noreply.github.com>
2022-05-12 15:45:55 -0300

    Variáveis e seus valores

--- Aula-01/script.js
+++ Aula-01/script.js
@@ -1,1 +1,14 @@
-alert("Tudo certo jovem");
+//tipos primitivos
+
+//boolean
+var vOUF = false;
+
+console.log(typeof(vOUF));
+
+//number
+var numeroQualquer = 1;
+console.log(typeof(numeroQualquer));
+
+//String
+var nome = 'Jane';
+console.log(typeof(nome));
